879b90365628ccf8522c711ca32e5d3db1356071
zzmkggtt6b-hue <zzmkggtt6b@privaterelay.appleid.com>
2026-09-04 22:40:48 -0300

    chore: configure ESLint

---
+++ eslint.config.mjs
@@ -0,0 +1,7 @@
+import { defineConfig } from "eslint/config";
+import nextVitals from "eslint-config-next/core-web-vitals";
+
+export default defineConfig([
+  ...nextVitals,
+  { ignores: [".next/**", "node_modules/**"] }
+]);
